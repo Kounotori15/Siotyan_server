9ea3d67a5ff1de1884920b58c51caaad2c68b5a0
Kounotori15 <waoon.kouno@nifty.com>
2018-12-16 11:32:26 +0900

    じゃんけん用のプログラムとindex.phpを分割しました

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+include_once "junken.php"
 $line_accessToken = getenv('LINE_BOT_TOKEN');
 $json_string = file_get_contents('php://input');
 $jsonObj = json_decode($json_string);
@@ -46,48 +47,5 @@ function send_message($replyToken, $line_accessToken, $message){
     curl_setopt_array($curl, $options);
     curl_exec($curl);
     curl_close($curl);
-}
-?>
-
-<?php
-function janken($message){
-    $a = rand(0,2);
-    if($message == "グー"){
-        if($a == 0){
-            $message = "グー....あいこだね、もう一回やるぞ";
-        }
-        if($a == 1){
-            $message = "チョキ....負けちゃた、次は負けないからね";
-        }
-        if($a == 2){
-            $message = "パー....やったー勝ったー、もう1回勝負だ";
-        }
-    }
-    else if($message == "チョキ"){
-        if($a == 0){
-            $message = "グー....やったー勝ったー、もう1回勝負だ";
-        }
-        if($a == 1){
-            $message = "チョキ....あいこだね、もう一回やるぞ";
-        }
-        if($a == 2){
-            $message = "パー....負けちゃた、次は負けないからね";
-        }
-    }
-    else if($message == "パー"){
-        if($a == 0){
-            $message = "グー....負けちゃた、次は負けないからね";
-        }
-        if($a == 1){
-            $message = "チョキ....やったー勝ったー、もう1回勝負だ";
-        }
-        if($a == 2){
-            $message = "パー....あいこだね、もう一回やるぞ";
-        }
-    }
-    else{
-        file_put_contents("data.txt", "0");
-    }
-    return $message;
 }
 ?>
---
+++ junken.php
@@ -0,0 +1,42 @@
+<?php
+function janken($message){
+    $a = rand(0,2);
+    if($message == "グー"){
+        if($a == 0){
+            $message = "グー....あいこだね、もう一回やるぞ";
+        }
+        if($a == 1){
+            $message = "チョキ....負けちゃた、次は負けないからね";
+        }
+        if($a == 2){
+            $message = "パー....やったー勝ったー、もう1回勝負だ";
+        }
+    }
+    else if($message == "チョキ"){
+        if($a == 0){
+            $message = "グー....やったー勝ったー、もう1回勝負だ";
+        }
+        if($a == 1){
+            $message = "チョキ....あいこだね、もう一回やるぞ";
+        }
+        if($a == 2){
+            $message = "パー....負けちゃた、次は負けないからね";
+        }
+    }
+    else if($message == "パー"){
+        if($a == 0){
+            $message = "グー....負けちゃた、次は負けないからね";
+        }
+        if($a == 1){
+            $message = "チョキ....やったー勝ったー、もう1回勝負だ";
+        }
+        if($a == 2){
+            $message = "パー....あいこだね、もう一回やるぞ";
+        }
+    }
+    else{
+        file_put_contents("data.txt", "0");
+    }
+    return $message;
+}
+?>
